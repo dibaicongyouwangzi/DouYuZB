863c698ddc8582904a970503483002ad6d4c36e3
LW <meipaigu@163.com>
2018-04-03 17:12:21 +0800

    布局推荐界面

--- DYZB/DYZB/Classes/Home/Controller/LWHomeViewController.swift
+++ DYZB/DYZB/Classes/Home/Controller/LWHomeViewController.swift
@@ -24,10 +24,11 @@ class LWHomeViewController: UIViewController {
     
     private lazy var pageContentView : LWPageContentView = { [weak self] in
         // 1.确定内容的frame
-        let contentFrame = CGRect(x: 0, y: kStatusBarH + kNavigationBarH + kTitleViewH, width: kScreenW, height: kScreenH - kStatusBarH - kNavigationBarH - kTitleViewH)
+        let contentFrame = CGRect(x: 0, y: kStatusBarH + kNavigationBarH + kTitleViewH, width: kScreenW, height: kScreenH - kStatusBarH - kNavigationBarH - kTitleViewH - kTabBarH)
         // 2.确定所有的子控制器
         var childVcs = [UIViewController]()
-        for _ in 0..<4 {
+        childVcs.append(LWRecommendViewController())
+        for _ in 0..<3 {
             let vc = UIViewController()
             vc.view.backgroundColor = UIColor(r: CGFloat(arc4random_uniform(255)), g: CGFloat(arc4random_uniform(255)), b: CGFloat(arc4random_uniform(255)))
             childVcs.append(vc)
---
+++ DYZB/DYZB/Classes/Home/Controller/LWRecommendViewController.swift
@@ -0,0 +1,92 @@
+//
+//  LWRecommendViewController.swift
+//  DYZB
+//
+//  Created by 迪拜葱油王子 on 2018/4/3.
+//  Copyright © 2018年 迪拜葱油王子. All rights reserved.
+//
+
+import UIKit
+
+private let kItemMargin : CGFloat = 10
+private let kItemW = (kScreenW - 3 * kItemMargin) / 2
+private let kItemH = kItemW * 3 / 4
+private let kHeaderViewH : CGFloat = 50
+
+private let kNormalCellID = "kNormalCellID"
+private let kHeaderViewID = "kHeaderViewID"
+
+class LWRecommendViewController: UIViewController {
+
+    // MARK:- 懒加载属性
+    private lazy var collectionView : UICollectionView = { [unowned self] in
+        // 1.创建布局
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: kItemW, height: kItemH)
+        layout.minimumLineSpacing = 0 // 行间距
+        layout.minimumInteritemSpacing = kItemMargin // item之间的间距
+        layout.headerReferenceSize = CGSize(width: kScreenW, height: kHeaderViewH)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: kItemMargin, bottom: 0, right: kItemMargin)
+        
+        // 2.创建UICollectionView
+        let collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
+        print(self.view.bounds)
+        collectionView.dataSource = self
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: kNormalCellID)
+        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: kHeaderViewID)
+        
+        return collectionView
+    }()
+    
+    // MARK:- 系统回调函数
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        view.backgroundColor = UIColor.brown
+        
+        // 设置UI界面
+        setupUI()
+    }
+}
+
+// MARK:- 设置UI界面内容
+extension LWRecommendViewController {
+    private func setupUI() {
+        // 1.将UICollectionView添加到控制器的view中
+        view.addSubview(collectionView)
+    }
+}
+
+// MARK:- 遵守UICollectionView的数据源协议
+extension LWRecommendViewController : UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 8
+        } else {
+            return 4
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // 1.获取cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kNormalCellID, for: indexPath)
+        
+        cell.backgroundColor = UIColor.red
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        // 1.取出sectionHeaderView
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: kHeaderViewID, for: indexPath)
+        
+        headerView.backgroundColor = UIColor.yellow
+        
+        return headerView
+    }
+}
