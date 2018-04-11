ee501c76f1304f50dff91f7a1a9b79caecf27219
LW <meipaigu@163.com>
2018-04-11 11:22:24 +0800

    娱乐界面-顶部详细布局

--- DYZB/DYZB/Classes/Home/View/LWAmuseMenuCell.swift
+++ DYZB/DYZB/Classes/Home/View/LWAmuseMenuCell.swift
@@ -1,5 +1,5 @@
 //
-//  LWAmuseMenuView.swift
+//  LWAmuseMenuCell.swift
 //  DYZB
 //
 //  Created by 迪拜葱油王子 on 2018/4/11.
@@ -8,43 +8,39 @@
 
 import UIKit
 
-private let kMenuCellID = "kMenuCellID"
+private let kGameCellID = "kGameCellID"
 
-class LWAmuseMenuView: UIView {
-    
+class LWAmuseMenuCell: UICollectionViewCell {
+
     // MARK:- 控件属性
     @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var pageControl: UIPageControl!
     
-    // MARK:- 从xib中加载出来
+    // MARK:- 从xib中加载
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        collectionView.register(UINib(nibName: "LWAmuseMenuCell", bundle: nil), forCellWithReuseIdentifier: kMenuCellID)
+        collectionView.register(UINib(nibName: "LWCollectionGameCell", bundle: nil), forCellWithReuseIdentifier: kGameCellID)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-        layout.itemSize = collectionView.bounds.size
+        
+        let itemW = collectionView.bounds.width / 4
+        let itemH = collectionView.bounds.height / 2
+        layout.itemSize = CGSize(width: itemW, height: itemH)
     }
 }
 
-extension LWAmuseMenuView {
-    class func amuseMenuView() -> LWAmuseMenuView {
-        return Bundle.main.loadNibNamed("LWAmuseMenuView", owner: nil, options: nil)?.first as! LWAmuseMenuView
-    }
-}
-
-extension LWAmuseMenuView : UICollectionViewDataSource {
+extension LWAmuseMenuCell : UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+        return 8
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         // 1.取出cell
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kMenuCellID, for: indexPath) as! LWAmuseMenuCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kGameCellID, for: indexPath) as! LWCollectionGameCell
         
         // 2.给cell设置数据
         cell.backgroundColor = UIColor.getRandomColor()
